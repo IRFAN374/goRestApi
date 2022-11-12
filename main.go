02c96d2fae80b0caaf5b446c9b22d79c46b4828e
irfanamu <irfan@valuecreatives.com>
2022-11-13 02:09:36 +0530

    router and handler is added

--- main.go
+++ main.go
@@ -13,5 +13,11 @@ func main() {
 
 	router := mux.NewRouter().StrictSlash(true)
 
+	router.HandleFunc("/allgrocery", AllGrocery).Methods("GET")
+	router.HandleFunc("/grocery/{name}", GetGrocery).Methods("GET")
+	router.HandleFunc("/grocery", AddGrocery).Methods("POST")
+	router.HandleFunc("/grocery/{name}", UpdateGrocery).Methods("PUT")
+	router.HandleFunc("/grocery/{name}", DeleteGrocery).Methods("DELETE")
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
