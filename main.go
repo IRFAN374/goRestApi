e311c6e7bdd20c17080c33bc6696cdf4e474e3da
irfanamu <irfan@valuecreatives.com>
2022-11-13 01:48:23 +0530

    model and mux is added

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
 	fmt.Println("Hello World")
+
+	router := mux.NewRouter().StrictSlash(true)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
